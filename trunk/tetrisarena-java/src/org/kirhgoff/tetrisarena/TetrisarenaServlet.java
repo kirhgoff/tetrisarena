836b934e346519f498bf5f772443b53403390229
kirill.lastovirya <kirill.lastovirya@7100a1aa-607a-11de-a6a3-193033c112f2>
2009-06-24 18:12:39 +0000

    tetrisarena-java added

---
+++ trunk/tetrisarena-java/src/org/kirhgoff/tetrisarena/TetrisarenaServlet.java
@@ -0,0 +1,11 @@
+package org.kirhgoff.tetrisarena;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TetrisarenaServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
